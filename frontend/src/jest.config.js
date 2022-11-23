1f51eac9efe2daee7dddd6ef650f21094e6056d0
fidellim <lim.fidel@gmail.com>
2022-11-24 01:18:43 +0400

    add test

---
+++ frontend/src/jest.config.js
@@ -0,0 +1,13 @@
+// const config = {
+//   verbose: true,
+//   jest: {
+//     setupFilesAfterEnv: ["src/setupTests.js"],
+//   },
+// };
+
+// module.exports = config;
+"use strict";
+module.exports = {
+  preset: "jest-puppeteer"
+};
+//# sourceMappingURL=jest.config.js.map
